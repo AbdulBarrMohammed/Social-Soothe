8c1f7bbfffcd7be68dacfb0c55c598640e5144eb
Barr Mohammed <barrmohammed@Barrs-MacBook-Air.local>
2024-09-24 22:14:12 -0400

    added question section in home jsx

--- social-soothe/src/components/home/Home.jsx
+++ social-soothe/src/components/home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Questions from './Questions'
 
 const Home = () => {
+
+
   return (
     <>
 
       <div className="bg-[#F9F6F4] flex flex-col items-center text-[#44423F]">
-        <div className='flex gap-10  justify-center items-center p-20'>
+        <div className='flex gap-10  justify-center items-center px-20 py-40'>
             <div className='w-2/4 flex gap-6 flex-col'>
               <h1 className='text-7xl font-semibold leading-[5rem] text-[#44423F]'>Helping You Navigate Social Anxiety.</h1>
               <p className='text-3xl leading-[2.6rem] text-[#44423F]'>Social Soothe will help with your social anxiety
@@ -22,7 +25,7 @@ const Home = () => {
         </div>
 
 
-        <div className='bg-white flex flex-wrap justify-center items-center p-20 gap-16 w-full'>
+        <div className='bg-white flex flex-wrap justify-center items-center px-20 py-40 gap-16 w-full'>
             <div className='flex flex-col gap-5 max-w-sm'>
               <div className='flex  items-center gap-5'>
                 <img src={'/public/write.svg'} className="h-32"/>
@@ -35,7 +38,7 @@ const Home = () => {
 
             <div className='flex flex-col gap-5 max-w-sm' >
               <div className='flex items-center gap-5'>
-                <img src={'/public/write.svg'} className="h-32"/>
+                <img src={'/public/yoga.svg'} className="h-32"/>
                 <h2 className='font-bold text-2xl'>Take some breathing exercises</h2>
               </div>
               <p className='text-lg'>Write down and keep a journal of how your feeling each to keep track of your mental health
@@ -45,7 +48,7 @@ const Home = () => {
 
             <div className='flex flex-col gap-5 max-w-sm'>
               <div className='flex items-center gap-5'>
-                <img src={'/public/write.svg'} className="h-32"/>
+                <img src={'/public/step.svg'} className="h-32"/>
                 <h2 className='font-bold text-2xl'>Keep track of goals</h2>
               </div>
               <p className='text-lg'>Write down and keep a journal of how your feeling each to keep track of your mental health
@@ -53,7 +56,42 @@ const Home = () => {
               </p>
             </div>
         </div>
-    </div>
+        <Questions />
+
+        <div className="bg-[#F9F6F4] flex flex-col justify-center items-center px-10 py-10 gap-10 w-full">
+          <h2 className='text-5xl text-center py-10 font-medium'>Take our Social Sooth challenge</h2>
+          <div className='flex flex-col gap-24 items-center justify-center'>
+
+            <div className='flex gap-20 items-center justify-center max-w-2xl'>
+                <img src={"/public/plant.svg"} className='w-1/3' />
+                <div className='flex flex-col gap-5'>
+                  <h2 className='text-3xl'>Start easy and slow</h2>
+                  <p className='text-2xl'>Our challenge will gradually give you social interactions through different levels, start with easy challenges that will slowly get your social juices flowing</p>
+                </div>
+            </div>
+
+            <img src={'/public/arrow-down.svg'} className='h-32'/>
+
+            <div className='flex gap-20 items-center justify-center max-w-2xl'>
+              <img src={"/public/plant.svg"} className='w-1/3' />
+              <div className='flex flex-col gap-5'>
+                <h2 className='text-3xl'>Grow gradually</h2>
+                <p className='text-2xl'>Each step will gradually have you socially interact more, with each level, social interactions will require more talking
+              and public exposure.</p>
+              </div>
+            </div>
+
+
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+
+
     </>
 
   )
---
+++ social-soothe/src/components/home/Questions.jsx
@@ -0,0 +1,169 @@
+import React, { useState } from 'react'
+
+const Questions = () => {
+    // use state to open social anxiety question drop down button
+  const [isDropdown, setIsDropdown] = useState(false); const [isDropdownTwo, setIsDropdownTwo] = useState(false); const [isDropdownThree, setIsDropdownThree] = useState(false);
+  const [isDropdownFour, setIsDropdownFour] = useState(false);
+  const [isDropdownFive, setIsDropdownFive] = useState(false);
+  const [isDropdownSix, setIsDropdownSix] = useState(false);
+  const [isDropdownSeven, setIsDropdownSeven] = useState(false);
+
+    return (
+        <div className='flex flex-col bg-white px-20 py-20 justify-center items-center w-full'>
+          <h2 className='text-5xl text-center leading-[4rem] py-16'>Social anxiety questions</h2>
+          <div className='p-3 border-y border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>What is social anxiety?</h2>
+              <button onClick={() => setIsDropdown((prev) => !prev)}>
+
+                {!isDropdown ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdown && (
+
+                <p>This works you were able to open the drop down option ! </p>
+              )}
+          </div>
+
+
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>What are the symptoms social anxiety?</h2>
+              <button onClick={() => setIsDropdownTwo((prev) => !prev)}>
+
+                {!isDropdownTwo ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownTwo && (
+
+                <p>This works you were able to open the drop down option ! </p>
+              )}
+          </div>
+
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>What are the causes of social anxiety?</h2>
+              <button onClick={() => setIsDropdownThree((prev) => !prev)}>
+
+                {!isDropdown ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownThree && (
+
+                <p>This works you were able to open the drop down option ! </p>
+              )}
+          </div>
+
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>How many people have social anxiety?</h2>
+              <button onClick={() => setIsDropdownFour((prev) => !prev)}>
+
+                {!isDropdownFour ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownFour && (
+                <p>This works you were able to open the drop down option ! </p>
+
+              )}
+          </div>
+
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>What treatments are available for social anxiety?</h2>
+              <button onClick={() => setIsDropdownFive((prev) => !prev)}>
+
+                {!isDropdownFive ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownFive && (
+                <p>This works you were able to open the drop down option ! </p>
+
+              )}
+          </div>
+
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>How is social anxiety diagnosed?</h2>
+              <button onClick={() => setIsDropdownSix((prev) => !prev)}>
+
+                {!isDropdownSix ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownSix && (
+                <p>This works you were able to open the drop down option ! </p>
+
+              )}
+          </div>
+          <div className='p-3 border-b border-[#C4C8C1] w-3/4 flex flex-col'>
+            <div className='flex justify-between items-center gap-10'>
+              <h2 className='text-xl'>Is social anxiety the same as shyness?</h2>
+              <button onClick={() => setIsDropdownSeven((prev) => !prev)}>
+
+                {!isDropdownSeven ? (
+                  <img src={'/public/chevron-down.svg'} className='h-10' />
+
+                ): (
+                  <img src={'/public/chevron-up.svg'} className='h-10' />
+
+                )}
+              </button>
+
+            </div>
+              {isDropdownSeven && (
+                <p>This works you were able to open the drop down option ! </p>
+
+              )}
+          </div>
+
+          <button className='mt-20 bg-[#EE7B30] text-white p-5 rounded-full text-[18px] font-bold shadow-md'>Get started</button>
+
+
+        </div>
+    )
+
+  }
+
+  export default Questions

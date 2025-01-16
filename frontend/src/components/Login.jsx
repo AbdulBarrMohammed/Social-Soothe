f5dfffd5a3956df33f735507165ba0f6bb62389a
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-01-16 14:30:00 -0500

    cors error demo

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ export function LogIn() {
   async function handleSubmit(e) {
     e.preventDefault()
 
-    const response = await fetch(`http://localhost:8000/login`, {
+    const response = await fetch(`https://social-soothe.onrender.com/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({email, password})
